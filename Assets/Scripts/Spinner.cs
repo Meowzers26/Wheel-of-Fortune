b8a5c16c334ac12ecf5cf38c7ab7af7ad591c139
Sophia <sophia.izwan@gmail.com>
2024-06-27 17:07:57 -0600

    Spin wheel

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float genSpeed;
+    public float subSpeed;
+    public bool isSpinning = false;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isSpinning == true) {
+            transform.Rotate(0, genSpeed, 0, Space.World);
+            genSpeed -= subSpeed;
+        }
+    }
+
+    public void spinWheel() {
+        genSpeed = Random.Range(2.000f, 5.000f);
+        subSpeed = Random.Range(0.003f, 0.009f);
+        isSpinning = true;
     }
 }
