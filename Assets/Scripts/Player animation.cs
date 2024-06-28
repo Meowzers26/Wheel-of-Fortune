2b1747ca01389f6e857a8fa5e4b34636225b7865
Sophia <sophia.izwan@gmail.com>
2024-06-27 18:25:11 -0600

    Trigger animation (doesn't work)

---
+++ Assets/Scripts/Player animation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playeranimatino : MonoBehaviour
+{
+
+    private Animator animator;
+    public Spinner ws;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator != null) {
+            if (ws.isSpinning == true) {
+                animator.SetTrigger("Celebrate");
+            }
+        }
+    }
+}
