37405ea353c7c2d684a3187a47c7522457670c1b
Sophia <sophia.izwan@gmail.com>
2024-06-29 00:05:04 -0600

    Add custom text to tiles

--- Assets/Scripts/Letters.cs
+++ Assets/Scripts/Letters.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Letters : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class Letters : MonoBehaviour
     public bool inUse = false;
     public Material lettersInUse;
     public Material lettersInDisuse;
+    public TextMeshPro outputLetter;
+    public TMP_InputField inputLetter;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,9 @@ public class Letters : MonoBehaviour
     {
         if (inUse == true) {
             GetComponent<MeshRenderer>().material = lettersInUse;
+
+            outputLetter.text = inputLetter.text;
+
         } else {
             GetComponent<MeshRenderer>().material = lettersInDisuse;
         }
